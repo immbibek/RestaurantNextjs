c1da840384fdf0e81c89219827f21593ca8a61df
Bibek <137331749+immbibek@users.noreply.github.com>
2024-10-27 23:43:25 +0530

    Menu bar for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,3 @@
-
-
-
 import Menu from './Menu'
 import Link from 'next/link'
 const Navbar = () => {
@@ -8,7 +5,7 @@ const Navbar = () => {
 
 
   return (
-    <div className='h-12 text-red-500 p-4 flex justify-between border-b-2- border-b-red-500 uppercase'>
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
       {/* logo */}
       <div className='text-xl'>
         <Link href="/">FoodMandu</Link>
