e96a6196130080a94d60e254bd6f309c21b1703a
Bibek <137331749+immbibek@users.noreply.github.com>
2024-10-28 00:34:42 +0530

    Navbar comp. Done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,40 @@
+import CartIcon from './CartIcon'
 import Menu from './Menu'
 import Link from 'next/link'
+import Image from 'next/image'
 const Navbar = () => {
-    
-
-
+    const user=false
   return (
-    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
-      {/* logo */}
-      <div className='text-xl'>
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-20 lg:px-20 xl:px-40">
+      {/* Left links */}
+      <div className='hidden md:flex gap-4 flex-1'>
+        <Link href="/">HomePage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Cotanct</Link>
+      </div>
+       {/* logo */}
+      <div className='text-xl md:font-bold flex-1 md:text-center'>
         <Link href="/">FoodMandu</Link>
       </div>
 
       {/* mobile menu */}
-      <div>
+      <div className='md:hidden'>
         <Menu/>
       </div>
+     
+     {/*Right Links  */}
+     <div className='hidden md:flex gap-4 items-center justify-end flex-1'>
+         <div className='md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
+            <Image src="/phone.png" alt='' width={20} height={20}/>
+            <span>122 3345 45</span>
+         </div>
+
+        {!user ? (<Link href="/login">Login</Link> )
+        :(
+        <Link href="/order">Order</Link>)}
+        <CartIcon/>
+      </div>
+      
     </div>
   )
 }
