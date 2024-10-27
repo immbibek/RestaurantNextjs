eeeb44e96b11fa028e6ba61d4ecc1acc2bb7802a
Bibek <137331749+immbibek@users.noreply.github.com>
2024-10-28 00:02:09 +0530

    mobile menu for moblie view done

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import CartIcon from './CartIcon'
 
 const links=[
     {id:1,title:"Homepage" ,url:"/"},
@@ -11,6 +12,8 @@ const links=[
 ]
 const Menu = () => {
   const [open,setOpen]=useState(false)
+  // temporary
+  const user=false;
   console.log(open);
   return (
     <div>
@@ -18,11 +21,22 @@ const Menu = () => {
       : (
       <Image src="/close.png" alt='' width={20} height={20}  onClick={()=>setOpen(false)}/>)}
 
-      <div className='bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl  z-10'>
+      {open && (<div className='bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-2xl  z-10'>
        {links.map(item=>(
-        <Link href={item.url} key={item.id}>{item.title}</Link>
+        <Link href={item.url} key={item.id} onClick={()=>setOpen(false)}>{item.title}</Link>
        ))}
-      </div>
+
+       {!user ?(<Link href="/login" onClick={()=>setOpen(false)}>Login</Link>
+       ) :(
+       <Link href="/orders" onClick={()=>setOpen(false)}>Orders</Link>
+       )}
+       
+       <Link href="/cart" onClick={()=>setOpen(false)}>
+         <CartIcon/>
+       </Link>
+
+      </div>)}
+
     </div>
   )
 }
