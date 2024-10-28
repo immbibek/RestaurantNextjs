28a00a6f26ef63d7a2b94ebf4e22a34675dab3c9
Bibek <137331749+immbibek@users.noreply.github.com>
2024-10-28 23:02:34 +0530

    Menu page is in progress

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import CountDown from './CountDown'
 const Offer = () => {
   return (
-    <div className=' bg-black h-screen flex flex-col md:flex-row'>
+    <div className=' bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url("/offerBg.png")] md:h-[70vh]'>
        {/* TEXT CONTAINER */}
 
        <div className='flex-1 flex flex-col justify-center items-center text-center gap-8 p-6'>
@@ -17,7 +17,7 @@ const Offer = () => {
        </div>
          {/* IMAGE CONTAINER */}
 
-       <div className='flex-1 w-full relative'>
+       <div className='flex-1 w-full relative md:h-full'>
          <Image src="/offerProduct.png" alt='' fill className='object-contain'/>
        </div>
 
